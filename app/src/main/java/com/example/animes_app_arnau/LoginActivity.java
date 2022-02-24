30b3a6ba6006d84942070de9bbe1fdc828971a62
PC-ARNAU\arnau <agraciataberner02@gmail.com>
2022-02-24 17:33:22 +0100

    .

--- app/src/main/java/com/example/animes_app_arnau/LoginActivity.java
+++ app/src/main/java/com/example/animes_app_arnau/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.animes_app_arnau;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,7 +24,13 @@ public class LoginActivity extends AppCompatActivity {
         //TODO: onClickListener btLogin2 --> LOGIN USER
 
         //TODO: onCLickListener btSignUp2 --> VIEW activity_signup
-
+        btSignUp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void hook() {
--- app/src/main/java/com/example/animes_app_arnau/SignupActivity.java
+++ app/src/main/java/com/example/animes_app_arnau/SignupActivity.java
@@ -2,9 +2,27 @@ package com.example.animes_app_arnau;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -15,6 +33,13 @@ public class SignupActivity extends AppCompatActivity {
     private Button btSignUp1;
     private Button btLogin1;
 
+    private static String CREATE_USER_URL = "https://joanseculi.com/edt69/createuser2.php";
+
+    private String nameV;
+    private String emailV;
+    private String passwordV;
+    private String phoneV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +47,74 @@ public class SignupActivity extends AppCompatActivity {
         hook();
 
         //TODO: onClickListener btLogin1 --> VIEW activity_login
-
+        btLogin1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         //TODO: onCLickListener btSignUp1 --> CREATE USER
+        btSignUp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!nameText.getText().toString().isEmpty() || !emailText.getText().toString().isEmpty()
+                        || !passText.getText().toString().isEmpty() || !phoneText.getText().toString().isEmpty()){
+                    createUser();
+                } else if (nameText.getText().toString().isEmpty()) {
+                    nameText.setError("Required");
+                } else if (emailText.getText().toString().isEmpty()) {
+                    nameText.setError("Required");
+                } else if (passText.getText().toString().isEmpty()) {
+                    nameText.setError("Required");
+                } else if (phoneText.getText().toString().isEmpty()) {
+                    nameText.setError("Required");
+                }
+            }
+        });
+    }
 
+    private void createUser() {
+        nameV = nameText.getText().toString();
+        emailV = emailText.getText().toString();
+        passwordV = passText.getText().toString();
+        phoneV = phoneText.getText().toString();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                CREATE_USER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT);
+                        if (response.equals("User created")){
+                            Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT);
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", nameV);
+                params.put("email", emailV);
+                params.put("password", passwordV);
+                params.put("phone", phoneV);
+                return params;
+            }
+        };
+        queue.add(stringRequest);
     }
 
     private void hook(){
